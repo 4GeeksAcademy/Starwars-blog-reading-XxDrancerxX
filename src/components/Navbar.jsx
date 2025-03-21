db8974fdcd90dc7018d68826d17d0628def2150c
XxDrancerxX <idmdiazmora98@gmail.com>
2025-03-21 20:51:12 +0000

    aa

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,34 @@
 import { Link } from "react-router-dom";
+const starWarsImageUrl = "https://upload.wikimedia.org/wikipedia/commons/b/b8/StarWarsLogo.jpeg";
 
 export const Navbar = () => {
 
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
+		<nav className="navbar bg-body-tertiary">
+			<div className="container-fluid">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<a className="navbar-brand"><img style={{width: "92px", height: "50px"}} src={starWarsImageUrl} alt="Star Wars Iocn"/></a>
 				</Link>
-				<div className="ml-auto">
+				<form className="d-flex" role="search">
 					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
+						<button classNameName="btn btn-primary">Check the Context in action</button>
 					</Link>
-				</div>
+					<div className="dropdown">
+						<a className="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+							Dropdown link
+						</a>
+
+						<ul className="dropdown-menu">
+							<li><a className="dropdown-item" href="#">Action</a></li>
+							<li><a className="dropdown-item" href="#">Another action</a></li>
+							<li><a className="dropdown-item" href="#">Something else here</a></li>
+						</ul>
+					</div>
+				</form>
 			</div>
-		</nav>
+
+		</nav >
 	);
 };
+
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,70 @@
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import { useEffect } from "react";
+
+
 
 export const Home = () => {
+	const { store, dispatch } = useGlobalReducer();
 
-  const {store, dispatch} =useGlobalReducer()
+	const baseUrl = "https://www.swapi.tech/api/" // or: https://swapi.tech/api/   //
+    
+	const getCharacters = () => {
+        fetch(`${baseUrl}people`)
+		.then((resp) =>{
+			console.log("All the Characters(rsp):", resp)
+			return resp.json()
+			
+		})
+		.then((data) => {
+			console.log("Data of Characters:", data);
+			return data
+		})
+    };
+	
+
+
+
+	const cardData = [
+		{
+			id: 1,
+			title: "Card title 1",
+			text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+			imageUrl: rigoImageUrl,
+		},
+		{
+			id: 2,
+			title: "Card title 2",
+			text: "Some more example text to build on the card title and make up the bulk of the card's content.",
+			imageUrl: rigoImageUrl,
+		}
+		
+	];
+	useEffect(() => {
+        getCharacters();
+    }, []);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
+		<div className="container">
+
+			<h1 className="text-center" style={{ fontSize: "40px", color: "red" }}>Characters</h1>
+			<div className="cards-characters  row">
+				{cardData.map((item) => (
+					<div key={item.id} className="card col-auto" style={{ width: "18rem", margin: "10px auto" }}>
+						<img src={item.imageUrl} className="card-img-top" alt={item.title} />
+						<div className="card-body">
+							<h5 className="card-title">{item.title}</h5>
+							<p className="card-text">{item.text}</p>
+							<a href="#" className="btn btn-primary">
+								Go somewhere
+							</a>
+						</div>
+					</div>
+				))}
+			</div>
+
+
+
 		</div>
 	);
-}; 
+};
